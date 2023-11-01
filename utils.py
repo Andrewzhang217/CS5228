65427c11d7ce580f95462a9ce8fac0b3588c3ae7
Andrewzhang217 <zhangguangzesheng@gmail.com>
2023-11-01 17:01:54 +0800

    Other changes

---
+++ utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    # Radius of the Earth in km
+    R = 6371.0
+
+    # Convert latitude and longitude from degrees to radians
+    lat1 = np.radians(lat1)
+    lon1 = np.radians(lon1)
+    lat2 = np.radians(lat2)
+    lon2 = np.radians(lon2)
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    distance = R * c
+
+    return distance
